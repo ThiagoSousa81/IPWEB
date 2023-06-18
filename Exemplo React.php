a5022fe2f0d48b79d906c577d0b233bfe644b97e
Thiago Sousa <thiagosousa81@gmail.com>
2023-06-17 21:29:14 -0300

    Update Exemplo React.php

--- Exemplo React.php
+++ Exemplo React.php
@@ -1,14 +1,23 @@
-<?php
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <title>Exemplo React</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  </head>  
+  <body>    
+    <?php
 // Gera alguns dados
-$nome = "João";
-$email = "joao@example.com";
+$nome = "Thiago";
+$email = "thiagosousa81@gmail.com";
 ?>
 
 <script src="https://cdn.jsdelivr.net/npm/react@17.0.2/umd/react.development.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/react-dom@17.0.2/umd/react-dom.development.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/6.26.0/babel.min.js"></script>
 
-<div id="app"></div>
+
 
 <script type="text/babel">
   // Define um componente React
@@ -30,3 +39,8 @@ $email = "joao@example.com";
     document.getElementById('app')
   );
 </script>
+
+    <!--A aplicação será mostrada aqui-->
+    <section id="app" class="container"></section>
+  </body>
+</html>  
